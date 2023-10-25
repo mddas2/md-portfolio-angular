ea9ca0d4d61012283a457fa81b535f351f86b308
mddas2 <manojdas.py@gmail.com>
2023-10-25 12:44:02 +0545

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { LeftbarComponent } from './core/leftbar/leftbar.component';
+import { RightbarComponent } from './core/rightbar/rightbar.component';
+import { ProfileComponent } from './core/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftbarComponent,
+    RightbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
